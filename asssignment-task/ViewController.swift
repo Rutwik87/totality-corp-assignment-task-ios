bbf90430285d65a6369b55fdd36f5f60e6fbcada
rutwik.shinde <rutwik.shinde@tatadigital.com>
2022-02-03 20:26:58 +0530

    Initial Commit

---
+++ asssignment-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  asssignment-task
+//
+//  Created by Rutwik Shinde on 03/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
